416af547ab12e88f38ffbc73fcc3e7c99969f741
ZZIDZZ <zidanutomo@gmail.com>
2023-06-05 18:36:11 +0700

    pbo

---
+++ Praktikum10/DiskonLambda.java
@@ -0,0 +1,26 @@
+interface IDiskon {
+    public double hitungDiskon(double harga);
+}
+
+
+public class DiskonLambda {
+    public static void main(String[] args){
+        // tanpa lambda
+        IDiskon diskonMerdeka = new IDiskon(){
+            public double hitungDiskon(double harga){
+                return harga - harga * 0.3;
+            }
+        };
+        // dengan lambda
+        IDiskon diskonHariRaya = (harga) -> harga - (harga * 0.4);
+        // dengan lambda dengan blok statement
+        IDiskon diskonNatal = (harga) -> {
+            double diskon = harga - (harga * 0.1);
+            return diskon;
+        };
+        // memanggil method
+        System.out.println("Harga setelah diskon merdeka: " + diskonMerdeka.hitungDiskon(100000.00));
+        System.out.println("Harga setelah diskon hari raya: " + diskonHariRaya.hitungDiskon(100000.00));
+        System.out.println("Harga setelah diskon natal: " + diskonNatal.hitungDiskon(100000.00));
+    }
+}
---
+++ Praktikum10/run-diskonlambda.ps1
@@ -0,0 +1,9 @@
+# compile and run DiskonLambda.java and all with package Praktikum7 all at the same directory
+
+# compile DiskonLambda.java
+javac -d bin DiskonLambda.java
+
+# run DiskonLambda
+java -cp bin DiskonLambda
+
+
